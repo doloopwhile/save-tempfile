4df09413ba8db58c6e8dd34f37d91359ab02e067
OMOTO Kenji <kenji.omoto@hde.co.jp>
2013-02-25 09:34:58 +0900

    Created anything

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ save-tempfile
 =============
 
 Save inputs to temporary file and print name of the temporary file
+
+```
+usage: save-tempfile.py [-h] [--suffix SUFFIX] [--prefix PREFIX] [--dir DIR]
+                        [FILES [FILES ...]]
+```
+
+positional arguments:
+  FILES
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --suffix SUFFIX  Suffix of name of temporary file
+  --prefix PREFIX  Prefix of name of temporary file
+  --dir DIR        Dir in which temporary created
---
+++ save-tempfile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import tempfile
+from argparse import ArgumentParser
+import fileinput
+
+description='Save inputs to temporary file and print name of the temporary file'
+parser = ArgumentParser(description=description)
+parser.add_argument('FILES', action='store', nargs='*')
+parser.add_argument('--suffix', action='store', default='',
+    help='Suffix of name of temporary file')
+parser.add_argument('--prefix', action='store', default='tmp',
+    help='Prefix of name of temporary file')
+parser.add_argument('--dir', action='store', default=tempfile.gettempdir(),
+    help='Dir in which temporary created')
+
+opts = parser.parse_args()
+kw = dict()
+for k in 'suffix prefix dir'.split():
+    kw[k] = getattr(opts, k)
+
+with tempfile.NamedTemporaryFile('w', delete=False, **kw) as fp:
+    fp.writelines(fileinput.input(opts.FILES))
+
+print(fp.name)
